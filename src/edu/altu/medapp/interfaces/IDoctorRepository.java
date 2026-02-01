57f0700272f256c076e0671fb99796660981f2fe
Avirup Neal <learnneal@gmail.com>
2026-02-01 21:01:24 +0500

    Avirup Roy - Added Generic Repository Interface; Updated Existing Interfaces; Updated Supabase Database

--- src/edu/altu/medapp/interfaces/IDoctorRepository.java
+++ src/edu/altu/medapp/interfaces/IDoctorRepository.java
@@ -2,14 +2,8 @@ package edu.altu.medapp.interfaces;
 
 import edu.altu.medapp.model.Doctor;
 import java.util.List;
-import java.util.Optional;
 
-public interface IDoctorRepository {
-    void save(Doctor doctor);
-    Optional<Doctor> findById(int id);
-    List<Doctor> findAll();
-    void update(Doctor doctor);
-    void delete(int id);
+public interface IDoctorRepository extends IRepository<Doctor> {
     List<Doctor> findBySpecialization(String specialization);
     List<Doctor> findAvailableDoctors();
 }
